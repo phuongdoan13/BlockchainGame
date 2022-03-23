af54490c490c53f3bf5c524ffd15c6872e40039c
phuongdoan13 <mphuongdoant@gmail.com>
2022-03-23 18:41:16 +1100

    Separate MemoryToken images dictionary into a separate file

---
+++ frontend/src/components/const/MemoryToken_const.js
@@ -0,0 +1,54 @@
+const CARD_ARRAY = [
+    {
+        name: 'fries',
+        img: '/Matching/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: '/Matching/cheeseburger.png'
+    },
+    {
+        name: 'ice-cream',
+        img: '/Matching/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: '/Matching/pizza.png'
+    },
+    {
+        name: 'milkshake',
+        img: '/Matching/milkshake.png'
+    },
+    {
+        name: 'hotdog',
+        img: '/Matching/hotdog.png'
+    },
+    {
+        name: 'fries',
+        img: '/Matching/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: '/Matching/cheeseburger.png'
+    },
+    {
+        name: 'ice-cream',
+        img: '/Matching/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: '/Matching/pizza.png'
+    },
+    {
+        name: 'milkshake',
+        img: '/Matching/milkshake.png'
+    },
+    {
+        name: 'hotdog',
+        img: '/Matching/hotdog.png'
+    }
+]
+
+
+export {CARD_ARRAY};
+
